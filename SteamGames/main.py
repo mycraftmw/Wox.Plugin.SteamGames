f34426ddae4f25fdc3c9921986ed0e52772461f9
Owen Worley <owen@owenworley.co.uk>
2019-06-14 12:46:50 +0100

    Refactors logic into a method that takes a directory to iterate. Allow config to have either a folder, or array of folders, array of folders will iterate ALL folders, for users that have multiple steam libraries.

--- SteamGames/main.py
+++ SteamGames/main.py
@@ -23,42 +23,12 @@ class Steamlauncher(Wox):
     if not dirConfig["steamapps_dir"]:
         steamappsDir = None
     else:
-        if os.path.isdir(dirConfig["steamapps_dir"]):
-            steamappsDir = dirConfig["steamapps_dir"]
-            for file_entry in os.scandir(steamappsDir):
-                if "appmanifest" in file_entry.name:
-                    gameId = file_entry.name.replace("appmanifest_", "").replace(
-                        ".acf", ""
-                    )
-                    if gameId == "228980":
-                        continue
-                    with open(file_entry.path) as f:
-                        for line in f:
-                            if line.find("name") > 0:
-                                gameTitle = (
-                                    line.replace('"', "").replace("name", "").strip()
-                                )
-                                break
-                    if os.path.isfile("./icon/" + gameId + ".jpg"):
-                        gameIcon = "./icon/" + gameId + ".jpg"
-                    else:
-                        try:
-                            url = "https://steamdb.info/app/{}/".format(gameId)
-                            headers = {
-                                "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"
-                            }
-                            r = requests.get(url, headers=headers)
-                            soup = BeautifulSoup(r.text, "html.parser")
-                            data = soup.find("img", attrs={"class": "app-icon avatar"})
-                            img = data.attrs["src"]
-                            urllib.request.urlretrieve(img, "./icon/" + gameId + ".jpg")
-                            gameIcon = "./icon/" + gameId + ".jpg"
-                            gameIcon = "./icon/missing.png"
-                        except Exception as e:
-                            gameIcon = "./icon/missing.png"
-                    gameList.append(
-                        {"gameId": gameId, "gameTitle": gameTitle, "gameIcon": gameIcon}
-                    )
+        if type(dirConfig["steamapps_dir"]) is list:
+            for directory in dirConfig["steamapps_dir"]:
+               if os.path.isdir(directory):
+                    collectGames(directory)
+        elif os.path.isdir(dirConfig["steamapps_dir"]):
+            collectGames(dirConfig["steamapps_dir"])
         else:
             steamappsDir = False
 
@@ -89,6 +59,43 @@ class Steamlauncher(Wox):
 
     def launchGame(self, gameId):
         webbrowser.open("steam://rungameid/{}".format(gameId))
+
+def collectGames(directory):
+    steamappsDir = directory
+    for file_entry in os.scandir(steamappsDir):
+        if "appmanifest" in file_entry.name:
+            gameId = file_entry.name.replace("appmanifest_", "").replace(
+                ".acf", ""
+            )
+            if gameId == "228980":
+                continue
+            with open(file_entry.path) as f:
+                for line in f:
+                    if line.find("name") > 0:
+                        gameTitle = (
+                            line.replace('"', "").replace("name", "").strip()
+                        )
+                        break
+            if os.path.isfile("./icon/" + gameId + ".jpg"):
+                gameIcon = "./icon/" + gameId + ".jpg"
+            else:
+                try:
+                    url = "https://steamdb.info/app/{}/".format(gameId)
+                    headers = {
+                        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0"
+                    }
+                    r = requests.get(url, headers=headers)
+                    soup = BeautifulSoup(r.text, "html.parser")
+                    data = soup.find("img", attrs={"class": "app-icon avatar"})
+                    img = data.attrs["src"]
+                    urllib.request.urlretrieve(img, "./icon/" + gameId + ".jpg")
+                    gameIcon = "./icon/" + gameId + ".jpg"
+                    gameIcon = "./icon/missing.png"
+                except Exception as e:
+                    gameIcon = "./icon/missing.png"
+            gameList.append(
+                {"gameId": gameId, "gameTitle": gameTitle, "gameIcon": gameIcon}
+            )
 
 
 if __name__ == "__main__":
